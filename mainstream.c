5c6bb2be7fa810d949454c4f3685e015012b4340
Reid Netterville III <reidiiius@gmail.com>
2020-09-13 15:39:12 -0500

    1600029552

--- mainstream.c
+++ mainstream.c
@@ -2,18 +2,10 @@
 
 #include "scordatura.h"
 
-#ifndef MUSKEY
-  #define MUSKEY 24
-#endif
-
-#ifndef CARLEN
-  #define CARLEN 80
-#endif
-
 
 int main(int argc, char **argv)
 {
-  fingerboard(argc, argv);
+  entryway(argc, argv);
 
   return 0;
 }
--- scordatura.h
+++ scordatura.h
@@ -19,8 +19,12 @@
 #define CARLEN 80
 
 
+void entryway(unsigned cargo, char **veils);
+
+void syllabus(void);
+
 void headstock(const char *sequ);
-void fingerboard(unsigned cargo, char **veils);
+
 char *pegbox(unsigned harp, const char *cart, char *yarn);
 
 
@@ -136,8 +140,16 @@ struct digraphs databank[] = {
 };
 
 
+#ifndef MUSKEY
+  #define MUSKEY 24
+#endif
 
-void fingerboard(unsigned cargo, char **veils)
+#ifndef CARLEN
+  #define CARLEN 80
+#endif
+
+
+void entryway(unsigned cargo, char **veils)
 {
   char clave[MUSKEY];
   unsigned niter, quant;
@@ -146,40 +158,44 @@ void fingerboard(unsigned cargo, char **veils)
   prop = databank;
 
   if (cargo > 1) {
-    char argot[MUSKEY];
-    unsigned found;
+    if (strlen(veils[1]) > 8) {
+      syllabus();
+    } else { 
+      char argot[MUSKEY];
+      unsigned found;
 
-    putchar('\n');
-    for (niter = 1; niter <= cargo-1; ++niter)
-    {
-      strncpy(argot, veils[niter], MUSKEY-1);
-      argot[MUSKEY-1] = '\0';
-      quant = strlen(argot);
-
-      if (quant > 1 && quant < MUSKEY/2) {
-        strcpy(clave, prop->signat);
-      } else {
-        printf("\t%s ?\n\n", argot);
-        continue;
-      }
-
-      while(strncmp(clave, "ETB", quant))
+      putchar('\n');
+      for (niter = 1; niter <= cargo-1; ++niter)
       {
-        found = 0;
+        strncpy(argot, veils[niter], MUSKEY-1);
+        argot[MUSKEY-1] = '\0';
+        quant = strlen(argot);
 
-        if (!strcmp(clave, argot)) {
-          printf("\t%s-i%lu\n", clave, epoch);
-          headstock(prop->course);
-          ++found;
-          break;
+        if (quant > 1 && quant < MUSKEY/2) {
+          strcpy(clave, prop->signat);
+        } else {
+          printf("\t%s ?\n\n", argot);
+          continue;
         }
 
-        strcpy(clave, (++prop)->signat);
+        while(strncmp(clave, "ETB", quant))
+        {
+          found = 0;
+
+          if (!strcmp(clave, argot)) {
+            printf("\t%s-i%lu\n", clave, epoch);
+            headstock(prop->course);
+            ++found;
+            break;
+          }
+
+          strcpy(clave, (++prop)->signat);
+        }
+
+        if (!found) printf("\t%s ?\n\n", argot);
+
+        prop = &databank[0];
       }
-
-      if (!found) printf("\t%s ?\n\n", argot);
-
-      prop = &databank[0];
     }
   } else {
     niter = 0;
@@ -204,16 +220,41 @@ void fingerboard(unsigned cargo, char **veils)
 } 
  
 
+void syllabus(void)
+{
+  unsigned crown, recount = sizeof(databank) / sizeof(databank[0]);
+  unsigned long epoch = time(NULL);
+  struct digraphs *prop;
+  prop = databank;
+
+  putchar('\n');
+  for (crown = 0; crown < recount-1; ++crown) {
+    printf("\t%s-i%lu\n", prop->signat, epoch);
+    headstock(prop->course);
+    ++prop;
+  }
+}
+
+
 /* designate tuning */
 void headstock(const char *sequ)
 {
-  static unsigned tuned[] = { BJ, FN, CN, GN, DN, AN, EN, BN, FK };
-  static unsigned notes = sizeof(tuned) / sizeof(tuned[0]);
-  unsigned pitch;
-  char wire[CARLEN];
+  unsigned narcs = strlen(sequ);
 
-  for (pitch = 0; pitch < notes; ++pitch)
-    puts(pegbox(tuned[pitch], sequ, wire));
+  if (narcs > 35 && narcs < 73) {
+    static unsigned tuned[] = { BJ, FN, CN, GN, DN, AN, EN, BN, FK };
+    static unsigned notes = sizeof(tuned) / sizeof(tuned[0]);
+    char wire[CARLEN];
+    unsigned pitch;
+
+    for (pitch = 0; pitch < notes; ++pitch)
+      puts(pegbox(tuned[pitch], sequ, wire));
+
+  } else {
+    printf("\tWidth: %u ?\n", narcs);
+    printf("\tFile: %s\n", __FILE__);
+    printf("\tLine: %d\n", __LINE__);
+  }
 
   putchar('\n');
 }
@@ -221,10 +262,16 @@ void headstock(const char *sequ)
 
 char *pegbox(unsigned harp, const char *cart, char *yarn)
 {
+  unsigned narcs = strlen(cart); 
   char cord[CARLEN];
 
-  strcpy(cord, cart + harp);
-  strncat(cord, cart, harp);
+  if (narcs > 35 && narcs < 73) {
+    strcpy(cord, cart + harp);
+    strncat(cord, cart, harp);
+  } else {
+    sprintf(cord, "Width: %u ?\n\tFile: %s\n\tLine: %d",
+      narcs, __FILE__, __LINE__);
+  }
 
   sprintf(yarn, "\t%s", cord);
   return yarn;
